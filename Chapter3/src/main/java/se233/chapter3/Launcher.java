1013757d55fce9c270aed1d4763214f601a1e38b
Migeleiei <seksanfresh043@gmail.com>
2023-07-28 17:24:54 +0700

    eiei

--- Chapter3/src/main/java/se233/chapter3/Launcher.java
+++ Chapter3/src/main/java/se233/chapter3/Launcher.java
@@ -2,11 +2,18 @@ package se233.chapter3;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Launcher extends Application {
     @Override
-    public void start(Stage stage)throws Exception{
+    public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(Launcher.class.getResource("main-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Indexer");
+        stage.setScene(scene);
+        stage.show();
     }
+    static void main(String[] args) { launch(args); }
 }
+
--- Chapter3/src/main/java/se233/chapter3/controller/MainViewController.java
+++ Chapter3/src/main/java/se233/chapter3/controller/MainViewController.java
@@ -1,4 +1,48 @@
 package se233.chapter3.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
+import javafx.scene.layout.Region;
+
+import java.io.File;
+
 public class MainViewController {
+    @FXML
+    private Region dropRegion;
+    @FXML
+    private ProgressBar progressBar;
+    @FXML
+    private ListView listView;
+    @FXML
+    public void initialize(){
+        dropRegion.setOnDragOver(dragEvent -> {
+            Dragboard db = dragEvent.getDragboard();
+            final boolean isAccepted = db.getFiles().get(0).getName().toLowerCase().endsWith(".pdf");
+            if(db.hasFiles() && isAccepted){
+                dragEvent.acceptTransferModes(TransferMode.COPY);
+            }else {
+                dragEvent.consume();
+            }
+        });
+    dropRegion.setOnDragDropped(dragEvent -> {
+        Dragboard db = dragEvent.getDragboard();
+        boolean success = false;
+        if(db.hasFiles()){
+            success = true;
+            String filePath;
+            int total_files = db.getFiles().size();
+            for(int i = 0; i < total_files; i++){
+                File file = db.getFiles().get(i);
+                filePath = file.getAbsolutePath();
+                System.out.println(filePath);
+            }
+            progressBar.setProgress(100);
+        }
+        dragEvent.setDropCompleted(success);
+        dragEvent.consume();
+    });
+    }
 }
